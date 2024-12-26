f8d827cec410ae5291e4fe3e581c43da2407f293
sakhilsaiteaj <sakhilsaiteaj@gmail.com>
2024-12-26 16:55:48 +0530

    backend

--- src/utils/animations.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-
-export const filterEndTracks = (animations) => {
-  return animations.map(clip => {
-    const filteredTracks = clip.tracks.filter(track => !track.name.includes('_end'));
-    return new THREE.AnimationClip(clip.name, clip.duration, filteredTracks);
-  });
-};
